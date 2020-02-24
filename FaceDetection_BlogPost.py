78b08a54b441cc81e2d2fdaad6e977f1063bcd4b
Pietra F T Madio <pietra.ferreira@embecosm.com>
2020-02-24 11:38:54 +0000

    Add files via upload

---
+++ FaceDetection_BlogPost.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+
+facial_cascade_classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade_classifier = cv2.CascadeClassifier("haarcascade_eye.xml")
+
+
+img = cv2.imread("image.jpg")
+img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x, y, w, h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h), (255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade_classifier.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
